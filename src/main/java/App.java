220661e04ffb67fe1d4277977ca2b228ebc883db
Jeffrey Hackford and Noah Kittleson <jeffrey@>
2016-04-27 09:45:08 -0700

    added lots of functionality for viewing tasks and mulitple tasks

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,19 +12,36 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("task", request.session().attribute("task"));
       model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/tasks/new", (request, response) ->  {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/task-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/tasks", (request, response) ->  {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("tasks", Task.all());
+      model.put("template", "templates/tasks.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     post("/tasks", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-
       String description = request.queryParams("description");
       Task newTask = new Task(description);
-      request.session().attribute("task", newTask);
-
       model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("tasks/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Task task = Task.find(Integer.parseInt(request.params(":id")));
+      model.put("task", task);
+      model.put("template", "templates/task.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
   }
