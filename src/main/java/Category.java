bcfd0260978a0f3067929ef0d2f7908a873cb336
Noah Kittleson and Nadiya Yeroshkina <nadiya@>
2016-04-28 09:26:16 -0700

    added tests in category for clearing and getting ID

--- src/main/java/Category.java
+++ src/main/java/Category.java
@@ -1,15 +1,29 @@
+import java.util.ArrayList;
+
 public class Category {
   String mName;
+  int mId;
+  private static ArrayList<Category> instances = new ArrayList<Category>();
 
   public Category(String name) {
     mName = name;
+    instances.add(this);
+    mId = instances.size();
   }
 
   public String getName() {
     return mName;
   }
 
-  public static void clear() {
+  public  static ArrayList<Category> all() {
+    return instances;
+  }
 
+  public static void clear() {
+    instances.clear();
+  }
+
+  public int getId() {
+    return mId;
   }
 }
--- src/test/java/CategoryTest.java
+++ src/test/java/CategoryTest.java
@@ -1,5 +1,7 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.ArrayList;
+
 
 public class CategoryTest {
   @After
@@ -18,5 +20,26 @@ public class CategoryTest {
   public void getName_categoryInstatiatesWithName_Home() {
     Category testCategory = new Category("Home");
     assertEquals("Home", testCategory.getName());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfCategory_true() {
+    Category firstCategory = new Category("Home");
+    Category secondCategory = new Category("Work");
+    assertTrue(Category.all().contains(firstCategory));
+    assertTrue(Category.all().contains(secondCategory));
+  }
+
+  @Test
+  public void clear_emptiesALlCategoriesFromList_0() {
+    Category testCategory = new Category("Home");
+    Category.clear();
+    assertEquals(Category.all().size(), 0);
+  }
+
+  @Test
+  public void getId_categoriesInstantiateWithAnId_1(){
+    Category testCategory = new Category("Home");
+    assertEquals(1, testCategory.getId());
   }
 }
